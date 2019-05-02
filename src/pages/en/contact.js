418bf32e2f2578c22da6688c50f7ef1c7e48588d
Victor Andrée <hello@victorandree.se>
2019-05-02 19:51:00 +0200

    feat: add rsvp to contact

--- src/pages/en/contact.js
+++ src/pages/en/contact.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import PageLayout from '../../layouts/PageLayout/en';
 import imageUrl from '../../images/contact.jpg';
+import { injectIntl } from 'react-intl';
+
+const rsvpHref = { id: 'rsvpHref' };
+const RsvpLink = injectIntl(({ intl }) => (
+  <a href={intl.formatMessage(rsvpHref)}>osa@victor-mal.in</a>
+));
 
 export default props => (
   <PageLayout
@@ -10,11 +16,15 @@ export default props => (
     imageUrl={imageUrl}
   >
     <dl>
-      <dt>Malin:</dt>
+      <dt>RSVP</dt>
+      <dd>
+        Please RSVP by June 30 to <RsvpLink />
+      </dd>
+      <dt>Malin</dt>
       <dd>
         <a href="tel:+467636010450">+46&nbsp;(0)73-601&nbsp;04&nbsp;50</a>
       </dd>
-      <dt>Victor:</dt>
+      <dt>Victor</dt>
       <dd>
         <a href="mailto:victor@victor-mal.in">victor@victor-mal.in</a> •{' '}
         <a href="tel:+46760025202">+46&nbsp;(0)76-002&nbsp;52&nbsp;02</a>
--- src/pages/kontakt.js
+++ src/pages/kontakt.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import PageLayout from '../layouts/PageLayout/sv';
 import imageUrl from '../images/contact.jpg';
+import { injectIntl } from 'react-intl';
+
+const rsvpHref = { id: 'rsvpHref' };
+const RsvpLink = injectIntl(({ intl }) => (
+  <a href={intl.formatMessage(rsvpHref)}>osa@victor-mal.in</a>
+));
 
 export default props => (
   <PageLayout
@@ -10,11 +16,15 @@ export default props => (
     imageUrl={imageUrl}
   >
     <dl>
-      <dt>Malin:</dt>
+      <dt>OSA</dt>
+      <dd>
+        O.s.a. senast den 30 juni till <RsvpLink />
+      </dd>
+      <dt>Malin</dt>
       <dd>
         <a href="tel:+467636010450">073-601&nbsp;04&nbsp;50</a>
       </dd>
-      <dt>Victor:</dt>
+      <dt>Victor</dt>
       <dd>
         <a href="mailto:victor@victor-mal.in">victor@victor-mal.in</a> •{' '}
         <a href="tel:+46760025202">076-002&nbsp;52&nbsp;02</a>
